352f24be757decf6e621baba3ea1ac4856032571
David Do <55934975+D-vidDo@users.noreply.github.com>
2020-02-11 13:15:36 -0700

    lab5 BIIIIIG progress

--- lab5/main.js
+++ lab5/main.js
@@ -1,24 +1,66 @@
 
 function test() {
-    var ele = document.getElementById('pizzasize').value; 
-      
-    for(i = 0; i < ele.length; i++) { 
-        if(ele[i].checked) 
-        document.getElementById("result").innerHTML
-                = "PizzaSize: "+ele[i].value;
 
-                var pizzaSize = ele[i].value;
+    // PIZZA CALCULATIONS
+    // pizza quantity
+    var pizzaQuantity = document.getElementById("pizzaNum").value;
+    document.getElementById("pizzaamounttotal").innerHTML = pizzaQuantity + " pizzas";
+
+    // select pizza size and multiply by quantity
+    var ele = document.getElementsByName('pizzasize');
+    for (i = 0; i < ele.length; i++) {
+        if (ele[i].checked) {
+            document.getElementById("firstresult").innerHTML = "Total before toppings: $" + (ele[i].value * pizzaQuantity);
+            var pizzaPrice = (ele[i].value * pizzaQuantity);
+        }
     }
 
+    // select toppings
+    var toppingsTotal = parseFloat(0);
+    var input = document.getElementsByName("toppings");
+    for (i = 0; i < input.length; i++) {
+        if (input[i].checked) {
+            toppingsTotal += parseFloat(input[i].value);
+            document.getElementById("toppingsresult").innerHTML = "Toppings Total: $" + toppingsTotal;
+        }
+    }
+    // sum of everything
+    document.getElementById("finalresult").innerHTML = "Grand Total: $" + (pizzaPrice+toppingsTotal);
+    /*
+    PIZZA VARIABLES:
+        pizzaQuantity = AMOUNT OF PIZZA
+        pizzaPrice = SIZE PRICE * QUANTITY
+        toppingsTotal = SUM OF ALL TOPPINGS
+    */
 
-    var pizzaNum = document.getElementById("pizzaNum").value;
+    // SANDWICH CALCULATIONS
+    // sandwich quantity
+    var sandNum = document.getElementById("sandNum").value;
 
-    var quan;
-    quan = parseFloat(pizzaNum);
-    var size;
-    size = parseFloat(pizzaSize);
+    // select sandwich flavour and multiply by quantity
+    var sand = document.getElementsByName('flavours');
+    for (i = 0; i < sand.length; i++) {
+        if (sand[i].checked) {
+            document.getElementById("sandwichresult").innerHTML = "Sandwich Price: $" + (sand[i].value * sandNum);
+            var sandwich = (sand[i].value);
+        }
+    }
+    /*
+    SANDWICH VARIABLES:
+        sandNum = AMOUNT OF SANDWICHES
+        sandwich = PRICE * QUANTITY
+    */
 
-    var total = size * quan;
+    // DRINK CALCULATIONS
+    // drink quantity
+    var drinkNum = document.getElementById("drinkNum").value;
 
-    document.getElementById("total").innerHTML = total;
+    // select drink and multiply by quantity
+    var drnk = document.getElementsByName('drinksize');
+    for (i = 0; i < drnk.length; i++) {
+        if (drnk[i].checked) {
+            document.getElementById("drinkresult").innerHTML = "Drinks Price: $" + (drnk[i].value * drinkNum);
+            var drink = (drnk[i].value);
+        }
+    }
 } 
