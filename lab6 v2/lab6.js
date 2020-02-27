a175c8f0c889339440e4f9e6d2d8e66a7588c678
David Do <55934975+D-vidDo@users.noreply.github.com>
2020-02-26 19:45:24 -0700

    lab6v2

--- lab6 v2/lab6.js
+++ lab6 v2/lab6.js
@@ -5,7 +5,7 @@ function convertImperialGallon(val) {
     //converts gallon to litre
     var gal = parseFloat(0);
     gal = val * 4.54;
-    //display in metric field
+    //display in metric number field
     document.getElementById("litre").value = gal.toFixed(2);
 }
 
@@ -13,12 +13,12 @@ function convertMetricLitre(val) {
     //converts litre to gallon
     var litre = parseFloat(0);
     litre = val / 4.54;
-    //display in imperial field
+    //display in imperial number field
     document.getElementById("gallon").value = litre.toFixed(2);
 }
 
 function convertImperialFeet(val) {
-    //converts feet to m
+    //converts feet to meter
     var m = parseFloat(0);
     m = val / 3.2808;
     //display in metric number field
@@ -26,15 +26,16 @@ function convertImperialFeet(val) {
 }
 
 function convertMetricMetre(val) {
-    //converts m to feet
+    //converts meter to feet
     var feet = parseFloat(0);
     feet = val * 3.2808;
+    // display in imperial number field
     document.getElementById("feet").value = feet.toFixed(2);
 
 }
 
 function convertImperialFeet2(val) {
-    //converts feet to m
+    //converts square feet to m
     var m2 = parseFloat(0);
     m2 = val / 10.7639;
     //display in metric number field
@@ -42,9 +43,26 @@ function convertImperialFeet2(val) {
 }
 
 function convertMetricMetre2(val) {
-    //converts m to feet
+    //converts square meter to feet
     var feet2 = parseFloat(0);
     feet2 = val * 10.7639;
+    // display in feet number field
     document.getElementById("feet2").value = feet2.toFixed(2);
 
+}
+
+function convertImperialPound(val) {
+    //converts pound to kilo
+    var pound = parseFloat(0);
+    pound = val / 2.205;
+    //display in metric number field
+    document.getElementById("kilogram").value = pound.toFixed(2);
+}
+
+function convertImperialKilogram(val) {
+    //converts kilo to pound
+    var kilo = parseFloat(0);
+    kilo = val * 2.205;
+    //display in imperial number field
+    document.getElementById("pound").value = kilo.toFixed(2);
 }
