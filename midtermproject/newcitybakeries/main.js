16e41350bdc661d27138c12132f86a19e27afb37
David Do <714163@ACDM.DS.SAIT.CA>
2020-03-05 17:08:05 -0700

    part 3

---
+++ midtermproject/newcitybakeries/main.js
@@ -0,0 +1,11 @@
+function test() {
+    var sheet = document.getElementById("sheetdim");
+    sheet.style.display = 'inline';
+    document.getElementById("rounddim").style.display = "none";
+}
+
+function test1() {
+    var round = document.getElementById("rounddim");
+    round.style.display = 'inline';
+    document.getElementById("sheetdim").style.display = "none";
+}
